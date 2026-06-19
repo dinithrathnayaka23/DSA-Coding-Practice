f78f33177621e1ac54e248f3799f89a60248a677
dinithrathnayaka23 <rathnayakarmdno.23@uom.lk>
2026-06-20 04:23:21 +0530

    Create Question01.java

---
+++ Beginner Level/Question01.java
@@ -0,0 +1,8 @@
+/*1. Student Marks Analyzer (Arrays)
+A school stores the marks of 50 students in an array. Write a program to calculate the highest mark, lowest mark, average mark, and count how many students scored above the average.*/
+
+public class Question01{
+    public static void main(String[]args){
+        
+    }
+}
