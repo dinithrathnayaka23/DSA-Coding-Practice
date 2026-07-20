8dc690cdf187a8ba4c4f822ab056080c9154866e
dinithrathnayaka23 <dinithoshada2003@gmail.com>
2026-07-20 15:09:05 +0530

    Completed the Decimal to Binary stack question

--- Exam Practice/Stack Problems/Question04.java
+++ Exam Practice/Stack Problems/Question04.java
@@ -3,10 +3,57 @@ Decimal to Binary Converter
 Using stack.
 */
 
+import java.util.Scanner;
 
+class NodeD{
+    int data;
+    NodeD next;
+
+    NodeD(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+
+class StackD{
+    NodeD top;
+
+    void push(int value){
+        NodeD newNode=new NodeD(value);
+        newNode.next=top;
+        top=newNode;
+        return;
+    }
+    int pop(){
+        if (isEmpty()) {
+            System.out.println("Stack underflown!");
+            return -1;
+        }
+        int value=top.data;
+        top=top.next;
+        return value;
+    }
+    boolean isEmpty(){
+        return top==null;
+    }
+}
 
 public class Question04 {
     public static void main(String[]args){
-
+        Scanner sc=new Scanner(System.in);
+        StackD obj=new StackD();
+        System.out.println("Enter a Decimal Number:");
+        int number=sc.nextInt();
+        int count=0;
+        while (number>0){
+            int remainder=number%2;
+            obj.push(remainder);
+            number=number/2;
+            count++;
+        }
+        System.out.println("Corresponding Binary Number is:");
+        for (int i =1; i <=count; i++) {
+            System.out.print(obj.pop());
+        }
     }
 }
