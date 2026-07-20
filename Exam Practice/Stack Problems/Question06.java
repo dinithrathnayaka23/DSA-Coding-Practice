6af5b6c792c4130ead58b8804d2916d8464c7764
dinithrathnayaka23 <dinithoshada2003@gmail.com>
2026-07-20 18:47:31 +0530

    Palindrome checker using stack added

--- Exam Practice/Stack Problems/Question06.java
+++ Exam Practice/Stack Problems/Question06.java
@@ -2,8 +2,58 @@
 Palindrome checker using stack
 */
 
+import java.util.Scanner;
+
+class NodeP{
+    char data;
+    NodeP next;
+
+    NodeP(char data){
+        this.data=data;
+        this.next=null;
+    }
+}
+
+class StackP{
+    NodeP top;
+
+    void push(char data){
+        NodeP newNode=new NodeP(data);
+        newNode.next=top;
+        top=newNode;
+        return;
+    }
+    char pop(){
+        if (isEmpty()) {
+            System.out.println("Stack Underflown!");
+            return '\0';
+        }
+        char data=top.data;
+        top=top.next;
+        return data;
+    }
+    boolean isEmpty(){
+        return top==null;
+    }
+}
+
 public class Question06 {
     public static void main(String[]args){
-
+        Scanner sc=new Scanner(System.in);
+        StackP obj=new StackP();
+        System.out.println("Enter the word:");
+        String word=sc.nextLine();
+        for (int i = 0; i < word.length(); i++) {
+            obj.push(word.charAt(i));
+        }
+        String reversedWord="";
+        for (int i = 0; i <word.length(); i++) {
+            reversedWord=reversedWord+obj.pop();
+        }
+        if (word.equals(reversedWord)) {
+            System.out.println("The Entered word is a Palindrome!");
+        } else{
+            System.out.println("The entered word is not a Palindrome!");
+        }
     }
 }
