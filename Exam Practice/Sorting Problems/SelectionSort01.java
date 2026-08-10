aa0acc34c6b78f17e5187dabff5f95d13e328f46
dinithrathnayaka23 <dinithoshada2003@gmail.com>
2026-08-11 01:07:46 +0530

    Added question 1,2,3 for Selection Sort

---
+++ Exam Practice/Sorting Problems/SelectionSort01.java
@@ -0,0 +1,30 @@
+public class SelectionSort01 {
+    public static void selectionSort(int arr[]){
+        for (int i = 0; i < arr.length-1; i++) {
+            int minIndex=i;
+
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j]<arr[minIndex]) {
+                    minIndex=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[minIndex];
+            arr[minIndex]=temp;
+
+        }
+    }
+
+    public static void main(String[]args){
+        int arr[]={5, 3, 8, 4, 2};
+        System.out.println("Original Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        selectionSort(arr);
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
