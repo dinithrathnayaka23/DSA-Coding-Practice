9f4de03ee4eb2994b9e667bdb9d5fbbd5b9087c1
dinithrathnayaka23 <dinithoshada2003@gmail.com>
2026-08-11 02:32:54 +0530

    selection sort question 7,8,9,10 question added

---
+++ Exam Practice/Sorting Problems/SelectionSort08.java
@@ -0,0 +1,27 @@
+public class SelectionSort08 {
+    public static void selectionSort(int arr[]){
+        for (int i = 0; i < arr.length-1; i++) {
+            int minIndex=i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j]<arr[minIndex]) {
+                    minIndex=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[minIndex];
+            arr[minIndex]=temp;
+        }
+    }
+    public static void main(String[]args){
+        int arr[]={14, 3, 11, 6, 8, 2, 10};
+        System.out.println("Original Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        selectionSort(arr);
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ Exam Practice/Sorting Problems/SelectionSort10.java
@@ -0,0 +1,27 @@
+public class SelectionSort10 {
+    public static void selectionSort(int arr[]){
+        for (int i = 0; i < arr.length-1; i++) {
+            int minIndex=i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j]<arr[minIndex]) {
+                    minIndex=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[minIndex];
+            arr[minIndex]=temp;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={18, 7, 25, 3, 12, 9, 1, 20};
+        System.out.println("Original Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        selectionSort(arr);
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
