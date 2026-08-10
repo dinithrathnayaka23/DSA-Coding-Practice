f4ae73a374f83e5f51607cdb6a581f33d811900f
dinithrathnayaka23 <dinithoshada2003@gmail.com>
2026-08-10 20:43:24 +0530

    Question 5,6,7 added to bubble sort

---
+++ Exam Practice/Sorting Problems/BubbleSort06.java
@@ -0,0 +1,27 @@
+public class BubbleSort06 {
+    public static void bubbleSort(int arr[]){
+        int n=arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j <n-1-i; j++) {
+                if (arr[j]>arr[j+1]) {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={20, 15, 5, 10, 25, 2};
+        System.out.println("Original Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        bubbleSort(arr);
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }    
+}
