43d6ad5a3fcf29b169c8ff977920bb92d1f145d6
dinithrathnayaka23 <dinithoshada2003@gmail.com>
2026-08-11 00:21:39 +0530

    Question 10 Bubble sort added

---
+++ Exam Practice/Sorting Problems/BubbleSort10.java
@@ -0,0 +1,28 @@
+public class BubbleSort10{
+    public static void bubbleSort(int arr[]){
+        int n=arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-1-i; j++) {
+                if (arr[j]>arr[j+1]) {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+
+                }
+            }
+        }
+    }
+
+    public static void main(String[]args){
+        int arr[]={18, 7, 25, 3, 12, 9, 1, 20};
+        System.out.println("Original Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        bubbleSort(arr);
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
